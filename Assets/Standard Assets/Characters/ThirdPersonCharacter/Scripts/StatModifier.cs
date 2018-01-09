bc943830ebf93c88834ff7f66516ee2ba0a1f3dc
thisguy17 <kylecohen7@gmail.com>
2018-01-09 11:25:04 -0800

    stats scripts
    added character stats script and related objects as scripts; added max base stats

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/StatModifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Standard_Assets.Characters.ThirdPersonCharacter.Scripts
+{
+    public class StatModifier
+    {
+        public float amount;
+        public float duration;
+    }
+}
