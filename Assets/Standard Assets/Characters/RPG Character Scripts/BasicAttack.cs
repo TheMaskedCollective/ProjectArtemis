4230049f39d034f863838409e4bc5e46208e9775
thisguy17 <kylecohen7@gmail.com>
2018-01-09 16:33:11 -0800

    combat functionality testing
    Added slider for healthbar to block object; added character attributes to max and block object (PracticeBlock); added functionality for dealing damage on collision trigger via axe object

---
+++ Assets/Standard Assets/Characters/RPG Character Scripts/BasicAttack.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttack : MonoBehaviour {
+
+    CharacterHealth targetHealth;
+    CharacterStats targetStats;
+    List<float> targetStatsList;
+    CharacterStats attackerStats;
+    List<float> attackerStatsList;
+    float attackerDamage;
+    AttackType m_AttackType;
+
+    void OnTriggerEnter (Collider col)
+    {
+        var weaponHolder = transform.parent.parent.gameObject;
+        attackerStats = weaponHolder.GetComponent<CharacterStats>();
+        attackerStatsList = attackerStats.GetCurrentStats();
+        if (attackerStats != null)
+        {
+            if (attackerStatsList[1] > attackerStatsList[0])
+            {
+                attackerDamage = attackerStatsList[1] * 2.5f;
+                m_AttackType = AttackType.precision;
+            }
+            else
+            {
+                attackerDamage = attackerStatsList[0] * 4f;
+                m_AttackType = AttackType.power;
+            }
+        }
+        if (col.gameObject.name == "PracticeBox")
+        {
+            targetStats = col.gameObject.GetComponent<CharacterStats>();
+            targetStatsList = targetStats.GetCurrentStats();
+            targetHealth = col.gameObject.GetComponent<CharacterHealth>();
+            if(targetHealth != null && !targetHealth.isDead)
+            {
+                if(m_AttackType == AttackType.precision)
+                {
+                    //precision attacks deal less damage but ignore constitutional defenses
+                    targetHealth.TakeDamage(attackerDamage + 500f);
+                }
+                else
+                {
+                    //power attacks are reduced (multiplied) by 100% (1) minus 5% (0.05) of, the defender's constitution minus 2.5% (0.025) of the attacker's strength
+                    targetHealth.TakeDamage(attackerDamage + 500f * (1 - (0.05f * (attackerStatsList[2] - (0.025f * attackerStatsList[0])))));
+                }
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+enum AttackType
+{
+    precision, power
+}
---
+++ Assets/Standard Assets/Characters/RPG Character Scripts/CharacterHealth.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class CharacterHealth : MonoBehaviour {
+
+    float m_StartingHealth;
+    float m_CurrentHealth;
+    CharacterStats m_CharacterStats;
+    List<float> m_CharacterStatsList;
+    public bool isDead;
+    bool damaged;
+    Slider healthSlider;
+
+	// Use this for initialization
+	void Start () {
+        m_CharacterStats = GetComponent<CharacterStats>();
+        m_CharacterStatsList = m_CharacterStats.GetCurrentStats();
+        m_StartingHealth = m_CharacterStatsList[2] * 100;
+        m_CurrentHealth = m_StartingHealth;
+        healthSlider = GetComponentInChildren<Slider>();
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = m_StartingHealth;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (damaged)
+        {
+            //show damage frames
+        }
+        damaged = false;
+	}
+
+    public void TakeDamage(float amount) //type and stats
+    {
+        //subtract damage from health
+        m_CurrentHealth -= amount;
+
+        //display changes in health bar
+        if (healthSlider != null)
+        {
+            healthSlider.value = m_CurrentHealth;
+        }
+
+        //indicate damage dealt
+        damaged = true;
+
+        //check for death
+        if(m_CurrentHealth < 1 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        isDead = true;
+        Destroy(transform.gameObject);
+        //display death results; disable character actions and/or player controls and display UI
+    }
+}
