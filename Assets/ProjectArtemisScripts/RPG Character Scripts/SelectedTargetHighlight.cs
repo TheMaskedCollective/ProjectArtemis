1e505ce92075f36cb84f6aea533830f5f373d590
thisguy17 <kylecohen7@gmail.com>
2018-01-17 16:23:57 -0800

    Targeting
    Added targeting system; highlights health bar of selected target; cannot select dead or previously selected target until other targets selected

---
+++ Assets/ProjectArtemisScripts/RPG Character Scripts/SelectedTargetHighlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedTargetHighlight : MonoBehaviour {
+
+    public GameObject highlight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.parent.GetComponentInParent<CharacterHealth>().m_IsCurrentTarget)
+        {
+            highlight.SetActive(true);
+        }
+        else
+        {
+            highlight.SetActive(false);
+        }
+	}
+}
