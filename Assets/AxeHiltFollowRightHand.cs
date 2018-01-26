dc30ac3c1125729307d49dc4538c4e237a43246a
thisguy17 <kylecohen7@gmail.com>
2018-01-26 09:38:36 -0800

    R1 Kyle on work pc

---
+++ Assets/AxeHiltFollowRightHand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeHiltFollowRightHand : MonoBehaviour {
+
+    Transform rightHand;
+    Transform axe;
+    Transform target;
+
+
+	// Use this for initialization
+	void Start () {
+        rightHand = GameObject.FindGameObjectWithTag("RightHand").transform;
+        axe = transform.parent.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        axe.transform.position = rightHand.transform.position - transform.localPosition;
+	}
+}
