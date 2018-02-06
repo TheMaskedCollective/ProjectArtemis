16ab713d6b20c86210e801ea6168628857f7a30c
thealmostcrimes <35125027+thealmostcrimes@users.noreply.github.com>
2018-02-06 00:09:13 -0600

    changed prefab models

---
+++ Assets/Scripts/Attack_Script.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_S : MonoBehaviour {
+
+	void Update ()
+	{
+		if(Input.GetKeyDown("x"))
+		{
+			//animation.Play("animation");
+		}
+	}
+}
